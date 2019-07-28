b20d50b04d35a21cb203e7b894d7b6c2e7262056
phi nguyen <vandevildog@gmail.comgit config --global user.email vandevildog@gmail.comgit commit -m 1st>
2019-07-27 19:16:18 -1000

    fin

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -12,11 +12,28 @@ module.exports = function(digits){
 
   // do your work here
 
+  function checkPalindrome(num) {
+    let numToString = num.toString();
+    const reverse = numToString.split('').reverse().join('');
+    
+    return numToString === reverse;
+  }
 
+  for (let i = 10 ** (digits - 1); i < 10 ** digits; i++) {
+    for (let j = 10 ** (digits - 1); j < 10 ** digits; j++) {
+      if (checkPalindrome(i * j)) {
+        if (i * j > palindromeNumber) {
+          palindromeNumber = i * j;
+          factor_0 = i;
+          factor_1 = j;
+        }
+      }
+    }
+  }
 
   return {
     factor_0 : factor_0,
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
-  };
-};
+  }
+}
